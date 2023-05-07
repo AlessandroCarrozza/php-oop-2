bfb39100937c50bc39efddac24fa4769b715573e
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-07 19:45:45 +0200

    Struttura base card

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Store OOP</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <h1 class="text-center p-4">My Pet Store OOP</h1>
+
+    <div class="container d-flex flex-wrap justify-content-around">
+        <div class="card mb-3" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">title</h5>
+                <p class="card-text">price</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">type</li>
+            </ul>
+        </div>
+
+    </div>
+</body>
+</html>
